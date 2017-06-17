98b0bfe8861228e9df0a0908943369d587d42d40
userv <rosen.t.angelov@gmail.com>
2017-06-17 18:52:28 +0300

    BashSoft Second Part

---
+++ BashSoft-SecondPart/BashSoft 6/BashSoft/IOManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BashSoft
+{
+    public static class IOManager
+    {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            var initialIdentation = path.Split('\\').Length;
+            var subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+            while (subFolders.Count != 0)
+            {
+                var currentPath = subFolders.Dequeue();
+                var identation = currentPath.Split('\\').Length - initialIdentation;
+                OutputWriter.WriteMessageOnNewLine($"{new string('-', identation)}{currentPath}");
+                foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                    subFolders.Enqueue(directoryPath);
+            }
+        }
+
+        public static void TraverseDirectory(int depth)
+        {
+            OutputWriter.WriteEmptyLine();
+            var initialIdentation = SessionData.currentPath.Split('\\').Length;
+            var subFolders = new Queue<string>();
+            subFolders.Enqueue(SessionData.currentPath);
+            while (subFolders.Count != 0)
+            {
+                var currentPath = subFolders.Dequeue();
+                var identation = currentPath.Split('\\').Length - initialIdentation;
+                if (depth - identation < 0)
+                    break;
+                OutputWriter.WriteMessageOnNewLine($"{new string('-', identation)}{currentPath}");
+
+                try
+                {
+                    foreach (var file in Directory.GetFiles(currentPath))
+                    {
+                        var indexOfLastSlash = file.LastIndexOf("\\");
+                        var fileName = file.Substring(indexOfLastSlash);
+                        OutputWriter.WriteMessageOnNewLine(new string('-', indexOfLastSlash) + fileName);
+                    }
+                    foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                        subFolders.Enqueue(directoryPath);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.UnautorizedAccessExceptionMessage);
+                }
+            }
+        }
+
+        public static void CreateDirectoryInCurrentFolder(string name)
+        {
+            var path = SessionData.currentPath + "\\" + name;
+            try
+            {
+                Directory.CreateDirectory(path);
+            }
+            catch (ArgumentException)
+            {
+                OutputWriter.DisplayException(ExceptionMessages.ForbiddenSymbolContainedInName);
+            }
+        }
+
+        public static void ChangeCurrentDirectoryRealative(string relativePath)
+        {
+            if (relativePath == "..")
+            {
+                try
+                {
+                    var currentPath = SessionData.currentPath;
+                    var indexOfLastSlash = currentPath.LastIndexOf("\\");
+                    var newPath = currentPath.Substring(0, indexOfLastSlash);
+                    SessionData.currentPath = newPath;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.UnableToGoHigherInPartitionHierarchy);
+                }
+            }
+            else
+            {
+                var currentPath = SessionData.currentPath;
+                currentPath += "\\" + relativePath;
+                ChangeCurrentDirectoryAbsolute(currentPath);
+            }
+        }
+
+
+        public static void ChangeCurrentDirectoryAbsolute(string absolutePath)
+        {
+            if (!Directory.Exists(absolutePath))
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidPath);
+                return;
+            }
+            SessionData.currentPath = absolutePath;
+        }
+    }
+}
---
+++ BashSoft-SecondPart/BashSoft 6/BashSoft/Tester.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using BashSoft;
+
+namespace SimpleJudge
+{
+    public static class Tester
+    {
+        public static void CompareContent(string userOutputPath, string expectedOutputPath)
+        {
+            OutputWriter.WriteMessageOnNewLine("Reading files...");
+            try
+            {
+                var mismatchPath = GetMismatchPath(expectedOutputPath);
+                var actualOutputLines = File.ReadAllLines(userOutputPath);
+                var expectedOutputLines = File.ReadAllLines(expectedOutputPath);
+                bool hasMismatch;
+
+                var mismatches =
+                    GetLinesWithPossibleMismatches(actualOutputLines, expectedOutputLines, out hasMismatch);
+                PrintOutput(mismatches, hasMismatch, mismatchPath);
+                OutputWriter.WriteMessageOnNewLine("File read!");
+            }
+            catch (FileNotFoundException)
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidPath);
+            }
+        }
+
+        private static void PrintOutput(string[] mismatches, bool hasMismatch, string mismatchPath)
+        {
+            if (hasMismatch)
+            {
+                foreach (var line in mismatches)
+                    OutputWriter.WriteMessageOnNewLine(line);
+                try
+                {
+                    File.WriteAllLines(mismatchPath, mismatches);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.InvalidPath);
+                }
+                return;
+            }
+            OutputWriter.WriteMessageOnNewLine("Files are identical. There are no mismatches.");
+        }
+
+        private static string[] GetLinesWithPossibleMismatches(string[] actualOutputLines, string[] expectedOutputLines,
+            out bool hasMismatch)
+        {
+            hasMismatch = false;
+            var output = string.Empty;
+            var mismatches = new string[actualOutputLines.Length];
+            OutputWriter.WriteMessageOnNewLine("Comparing files...");
+            var minOutputLines = actualOutputLines.Length;
+            if (actualOutputLines.Length != expectedOutputLines.Length)
+            {
+                hasMismatch = true;
+                minOutputLines = Math.Min(actualOutputLines.Length, expectedOutputLines.Length);
+                OutputWriter.DisplayException(ExceptionMessages.ComparisonFilesWithDifferentSizes);
+            }
+            for (var index = 0; index < minOutputLines; index++)
+            {
+                var actualLine = actualOutputLines[index];
+                var expectedLine = expectedOutputLines[index];
+                if (!actualLine.Equals(expectedLine))
+                {
+                    output = $"Mismatch at line {index} -- expected: \"{expectedLine}\", actual: \"{actualLine}\"";
+                    output += Environment.NewLine;
+                    hasMismatch = true;
+                }
+                else
+                {
+                    output = actualLine;
+                    output += Environment.NewLine;
+                }
+                mismatches[index] = output;
+            }
+
+            return mismatches;
+        }
+
+        private static string GetMismatchPath(string expectedOutputPath)
+        {
+            var indexOf = expectedOutputPath.LastIndexOf('\\');
+            var directoryPath = expectedOutputPath.Substring(0, indexOf);
+            var finalPath = directoryPath + @"\Mismatches.txt";
+            return finalPath;
+        }
+    }
+}
